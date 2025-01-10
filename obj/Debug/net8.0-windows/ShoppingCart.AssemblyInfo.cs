e9c3656f980070c4deeeb115f3cc0a4b9111ae34
panuwat.prasertdee <panuwat.pra@kkumail.com>
2025-01-10 19:30:55 +0700

    work

--- obj/Debug/net8.0-windows/ShoppingCart.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ShoppingCart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477ea70b852acf64c33335a0c15d9d606680755c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
